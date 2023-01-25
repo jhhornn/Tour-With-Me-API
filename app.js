6d0d1f31350a9c2abff07fd90e57b5cd5bcb88fc
Awosise Oluwaseun <awosiseo@gmail.com>
2023-01-25 21:02:45 +0100

    chore: installed and used some third party middlewares

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const morgan = require("morgan")
 const tourRouter = require("./routes/tourRoutes")
 const userRouter = require("./routes/userRoutes")
 
 const app = express()
+
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.static(`${__dirname}/public`))
 
 app.use("/api/v1/tours", tourRouter)
 app.use("/api/v1/users", userRouter)
