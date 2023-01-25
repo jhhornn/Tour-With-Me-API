25702381ccc3c3c298730cd7bf2c1f6ab97e4aca
Awosise Oluwaseun <awosiseo@gmail.com>
2023-01-25 18:32:52 +0100

    chore: server setup moved to servver setup moved to server file and npm script setup to run server

--- app.js
+++ app.js
@@ -11,8 +11,4 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello from the server side')
 })
 
-const PORT = 3000
-
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}`)
-})  
+module.exports = app  
