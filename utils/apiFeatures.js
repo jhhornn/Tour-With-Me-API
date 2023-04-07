4f331a972625053e6003f37daa73cab8d42e6e60
Awosise Oluwaseun <awosiseo@gmail.com>
2023-04-07 12:08:43 +0100

    feat: filter feature using mongoose comparison operator query method

---
+++ utils/apiFeatures.js
@@ -0,0 +1,23 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((field) => delete queryObj[field]);
+
+    // Advanced query
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
