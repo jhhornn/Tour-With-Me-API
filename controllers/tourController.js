1c613020fda1a05602cd80308646dd64caf4ab34
Awosise Oluwaseun <awosiseo@gmail.com>
2023-04-12 12:25:33 +0100

    feat: createTour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,3 +25,20 @@ exports.getAllTours = async (req, res) => {
     });
   }
 };
+
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      Message: err.message,
+    });
+  }
+};
