b7d94b2d4899b96470b4206257aa9dd12c57bab8
Awosise Oluwaseun <awosiseo@gmail.com>
2023-04-20 12:36:16 +0100

    feat: document middleware to add slug before saving

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const slugify = require('slugify');
+const slugify = require('slugify');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -86,6 +86,16 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+//DOCUMENT MIDDLEWARE: runs before the .save() and .create() and not before .insertMany()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.post('save', function (doc, next) {
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
