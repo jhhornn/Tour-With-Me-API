98a55602733d92008ffb6be376cfb25549bafde4
Awosise Oluwaseun <awosiseo@gmail.com>
2023-04-21 12:38:12 +0100

    feat: query middleware to remove secret tour and calculate query time before returning data

--- models/tourModel.js
+++ models/tourModel.js
@@ -97,6 +97,20 @@ tourSchema.pre('save', function (next) {
 //   next();
 // });
 
+// QUERY MIDDLEWARE
+// Regex: All strings that start with find
+tourSchema.pre(/^find/, function (next) {
+  // tourSchema.pre('find', (next) => {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
